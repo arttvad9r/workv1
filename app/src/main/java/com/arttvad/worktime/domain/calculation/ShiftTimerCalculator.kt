94e46562fd90fb7dddb58e1dc077ec27016c1e9f
arttvad9r <arttvad9r@gmail.com>
2026-09-09 00:54:34 +0300

    feat: add persisted shift timer calculation

---
+++ app/src/main/java/com/arttvad/worktime/domain/calculation/ShiftTimerCalculator.kt
@@ -0,0 +1,17 @@
+package com.arttvad.worktime.domain.calculation
+
+object ShiftTimerCalculator {
+    const val MAX_TRACKED_MINUTES: Int = 99 * 60 + 59
+
+    fun elapsedMinutes(
+        startedAtEpochMillis: Long,
+        endedAtEpochMillis: Long,
+    ): Int? {
+        if (startedAtEpochMillis < 0L || endedAtEpochMillis <= startedAtEpochMillis) return null
+
+        val elapsedMinutes = (endedAtEpochMillis - startedAtEpochMillis) / 60_000L
+        if (elapsedMinutes !in 1L..MAX_TRACKED_MINUTES.toLong()) return null
+
+        return elapsedMinutes.toInt()
+    }
+}
